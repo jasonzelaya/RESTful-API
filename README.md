a29b6cd6d963e89d1b8f953b8fd5ef76e61e8a18
FirstWisdom <jason.zelaya21@yahoo.com>
2019-09-16 19:52:38 -0700

    Add boilerplate code to app.js and Technologies to README

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # RESTful API
+
+#### --Status: Active
+
+### Technologies
+* Node.js
+* JavaScript
+* EJS
+* Express
+* Body-Parser
+* MongoDB
+* Mongoose
+* Robo 3T
+* Postman
---
+++ app.js
@@ -0,0 +1,50 @@
+// Incorporate modules
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+// Initiate app
+const app = express();
+
+// Use EJS
+app.set("view engine", ejs);
+
+// Use body-parser
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Connect to MongoDB
+mongoose.connect("mongodb://localhost:27017/wikiDB", {
+  // Fix the URL string parser deprecation warning
+ useNewUrlParser: true,
+ // Fix Server Discovery and Monitoring Engine deprecation warning
+ useUnifiedTopology: true
+});
+
+// -----------------------------------------------------------------------------
+
+// Create schema
+const articlesSchema = mongoose.Schema({
+  title: String,
+  content: String
+});
+
+// Create model
+const Article = mongoose.model("Article", articlesSchema);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Start server
+app.listen(3000, function(){
+  console.log("Server is running on port 3000");
+});
